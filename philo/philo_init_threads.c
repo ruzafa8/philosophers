05ac6e11830422fd88cb4c1be2caf750c053acb2
Alejandro jose Ruzafa casas <aruzafa-@c3r2s4.42malaga.com>
2023-05-01 21:11:39 +0200

    Philosophers first eat/think

--- philo/philo_init_threads.c
+++ philo/philo_init_threads.c
@@ -6,13 +6,13 @@
 /*   By: aruzafa- <aruzafa-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/01 17:18:29 by aruzafa-          #+#    #+#             */
-/*   Updated: 2023/05/01 20:53:36 by aruzafa-         ###   ########.fr       */
+/*   Updated: 2023/05/01 21:08:09 by aruzafa-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-static void	philo_print(t_philo *philo, char *status)
+void	philo_print(t_philo *philo, char *status)
 {
 	u_int64_t	time;
 
@@ -22,23 +22,54 @@ static void	philo_print(t_philo *philo, char *status)
 	pthread_mutex_unlock(&(philo->data->mutex_print));
 }
 
-static void	try_take_fork(t_philo *philo)
+static int	try_take_fork(int id, t_philo *philo)
 {
+	int	taken;
+
 	pthread_mutex_lock(&(philo->mutex_fork));
+	taken = 0;
 	if (!philo->fork)
 	{
 		philo->fork = 1;
-		philo_print(philo, "soy subnormal");
+		pthread_mutex_lock(&(philo->data->mutex_print));
+		printf("%-6llu %-3d chopstick %d taken.\n", philo_current_time() - philo->data->t0, id, philo->id);
+		pthread_mutex_unlock(&(philo->data->mutex_print));
+		taken = 1;
 	}
 	pthread_mutex_unlock(&(philo->mutex_fork));
+	return (taken);
 }
 
 static void	*philo_routine(void *params)
 {
 	t_philo	*philo;
+	t_philo *next_philo;
+	int		taken;
+	int		taken2;
 
 	philo = (t_philo *) params;
-	try_take_fork(philo);
+	next_philo = philo->next;
+	if (next_philo == 0)
+		next_philo = philo->data->philos;
+	if (philo->id % 2 == 0)
+		taken = try_take_fork(philo->id, next_philo);
+	else
+		taken = try_take_fork(philo->id, philo);
+	if (taken)
+	{
+		if (philo->id % 2 == 0)
+			taken2 = try_take_fork(philo->id, philo);
+		else
+			taken2 = try_take_fork(philo->id, next_philo);
+	}
+	if (taken && taken2)
+	{
+		philo_print(philo, "is eating");
+	}
+	else
+	{
+		philo_print(philo, "is thinking");
+	}
 	//philo_print(philo, "Am alive!!!!!!!!!!!!!");
 	return (0);
 }
